9538efb0ee732b019bce9f036df60f15d52b98a7
Henk Jan van Wijk <wijkjh@gmail.com>
2016-10-18 16:08:24 +0200

    Removed url from example Python script

--- test_authorapi.py
+++ test_authorapi.py
@@ -8,7 +8,7 @@ def pp_json(json_thing, sort=True, indents=4):
         print(json.dumps(json_thing, sort_keys=sort, indent=indents))
     return None
 
-req = Request('https://private-bd9a0-authorapi.apiary-mock.com/authors?start=1&size=10&sort=id')
+req = Request('https://xxxxxx.apiary-mock.com/authors?start=1&size=10&sort=id')
 req.add_header('Authorization', 'Basic dXNlcm5hbWU6cGFzc3dvcmQ=')
 req.add_header('x-api-version', '1.0')
 resp = urlopen(req).read().decode("utf-8") 
