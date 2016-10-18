0b5328569ddc863d59dc1e49b05fb4e3310b1c64
Henk Jan van Wijk <wijkjh@gmail.com>
2016-10-18 16:07:27 +0200

    Added Puthon test api for calling Apiary mock

---
+++ test_authorapi.py
@@ -0,0 +1,16 @@
+from urllib.request import Request, urlopen
+import json
+
+def pp_json(json_thing, sort=True, indents=4):
+    if type(json_thing) is str:
+        print(json.dumps(json.loads(json_thing), sort_keys=sort, indent=indents))
+    else:
+        print(json.dumps(json_thing, sort_keys=sort, indent=indents))
+    return None
+
+req = Request('https://private-bd9a0-authorapi.apiary-mock.com/authors?start=1&size=10&sort=id')
+req.add_header('Authorization', 'Basic dXNlcm5hbWU6cGFzc3dvcmQ=')
+req.add_header('x-api-version', '1.0')
+resp = urlopen(req).read().decode("utf-8") 
+
+pp_json(resp)
